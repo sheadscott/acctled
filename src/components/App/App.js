4762fdbb263dbac0b5399aee9c830b9be8372b65
Robert Ferguson <rferguso@austincc.edu>
2018-12-05 15:28:52 -0600

    add url parameters to router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-
-import logo from '../../img/logo.svg';
 import './App.css';
+
 import TitleBar from '../TitleBar/TitleBar';
 import SecondaryNav from '../SecondaryNav/SecondaryNav';
 
@@ -16,23 +15,17 @@ class App extends Component {
           <header className="App-header">
             <TitleBar />
             <SecondaryNav />
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-          </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-          </a>
           </header>
 
 
-          <Route path="/" exact component={Page} />
-          <Route path="/test/:slug" component={Page} />
+          <main>
+            <Route path="/" exact component={Page} />
+            <Route path="/test/:slug" component={Page} />
+          </main>
+
+          <footer>
+            This is the footer.
+          </footer>
         </div>
       </Router>
     );
--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
-export default function Page() {
+export default function Page(props) {
   return (
     <div style={{ padding: '3rem' }}>
       <h1>This is a page component</h1>
+      {props.match.params.slug && (<p>
+        Slug: {props.match.params.slug}
+      </p>)}
     </div>
   )
 }
