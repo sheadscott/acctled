38c658ab9d16891b47974f374f2c1298f5504be7
Robert Ferguson <rferguso@austincc.edu>
2018-12-05 16:16:55 -0600

    add content ajax to page component

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react';
+import Axios from 'axios';
 
-export default function Page(props) {
-  return (
-    <div style={{ padding: '3rem' }}>
-      <h1>This is a page component</h1>
-      {props.match.params.slug && (<p>
-        Slug: {props.match.params.slug}
-      </p>)}
-    </div>
-  )
+export default class Page extends Component {
+  state = {
+    pageContent: null
+  }
+  componentDidMount() {
+    if (this.props.match.params.slug) {
+      Axios.get(`https://instruction.austincc.edu/tled/wp-json/wp/v2/pages?slug=${this.props.match.params.slug}`).then(response => {
+        console.log('response data', response.data);
+
+        this.setState({
+          pageContent: response.data[0]
+        })
+      });
+    }
+  }
+  render() {
+    const pageContent = this.state.pageContent
+    return (
+      <div style={{ padding: '3rem' }}>
+        <h1>This is a page component</h1>
+        {this.props.match.params.slug && (<div>
+          <h2>{pageContent && pageContent.title.rendered}</h2>
+          Slug: {this.props.match.params.slug}
+        </div>)}
+      </div>
+    )
+  }
 }
