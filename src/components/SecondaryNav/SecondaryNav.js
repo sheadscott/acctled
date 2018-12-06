161696f7b8bac086cf0ade5e5233d3b7d28b7741
Robert Ferguson <rferguso@austincc.edu>
2018-12-06 14:34:29 -0600

    start secondary nav component

--- src/components/SecondaryNav/SecondaryNav.js
+++ src/components/SecondaryNav/SecondaryNav.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
-
+import { Link } from "react-router-dom";
+import styled from 'styled-components';
 /*
   This component should go under the title bar
   according to the comp is has menu items like: about, faculty support, champions of teaching, etc
@@ -9,16 +10,60 @@ import Axios from 'axios';
 */
 
 export default class SecondaryNav extends Component {
+  state = {
+    data: []
+  }
   componentDidMount() {
     Axios.get('https://instruction.austincc.edu/tled/wp-json/wp-api-menus/v2/menus/4').then(response => {
       console.log('secondary menu data', response.data);
+      this.setState({
+        data: response.data.items
+      })
     });
   }
   render() {
     return (
-      <div>
-        Secondary Nav
-      </div>
+      <Wrapper>
+        <nav>
+          <NavList>
+            {this.state.data.map(item => {
+
+              // Internal links using React Router
+              if (item.type === "post_type") {
+                return <ListItem><Link key={item.id} to={`/${item.object_slug}`}>{item.title}</Link></ListItem>;
+              }
+
+              // external links
+              return <ListItem><a key={item.id} href={item.url}>{item.title}</a></ListItem>
+            })}
+          </NavList>
+        </nav>
+      </Wrapper>
     )
   }
 }
+
+const Wrapper = styled.div`
+  background: #133952;
+  padding: 1rem
+`;
+
+const NavList = styled.ul`
+  padding: 0;
+  margin: 0 -0.5rem;
+  list-style: none;
+  display: flex;
+`;
+
+const ListItem = styled.li`
+  margin: 0 0.5rem;
+  a {
+    color: #f1ebab;
+    text-decoration: none;
+  }
+
+  a:hover,
+  a:focus {
+    text-decoration: underline;
+  }
+`;
