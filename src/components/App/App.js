c6d9e532311de0e37ad1edb184b92feedc8cac97
sheadscott <sheadscott@gmail.com>
2018-12-06 09:27:00 -0600

    Added Footer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import './App.css';
+import "./App.css";
 
-import TitleBar from '../TitleBar/TitleBar';
-import SecondaryNav from '../SecondaryNav/SecondaryNav';
+import TitleBar from "../TitleBar/TitleBar";
+import SecondaryNav from "../SecondaryNav/SecondaryNav";
+import Footer from "../Footer/Footer";
 
-import Page from '../Page/Page';
+import Page from "../Page/Page";
 
 class App extends Component {
   render() {
@@ -17,15 +18,12 @@ class App extends Component {
             <SecondaryNav />
           </header>
 
-
           <main>
             <Route path="/" exact component={Page} />
             <Route path="/test/:slug" component={Page} />
           </main>
 
-          <footer>
-            This is the footer.
-          </footer>
+          <Footer />
         </div>
       </Router>
     );
