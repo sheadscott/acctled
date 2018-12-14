1af6fda7acb95bffb4b12b0d5d14ae6f0c72d55d
Robert Ferguson <rferguso@austincc.edu>
2018-12-14 12:32:20 -0600

    init sidebar template layouts

--- src/components/Pages/WPPage.js
+++ src/components/Pages/WPPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Axios from 'axios';
 
-import { Container } from '../Grid/Grid';
+import { Container, Row, Column } from '../Grid/Grid';
 import ACF from '../ACF/ACF';
 import Section from '../Section/Section';
 import SectionHeading from '../SectionHeading/SectionHeading';
@@ -45,8 +45,61 @@ export default class WPPage extends Component {
       <div>
         <Section>
           <Container>
-            <SectionHeading>{pageContent && pageContent.title.rendered}</SectionHeading>
-            {pageContent && <section dangerouslySetInnerHTML={{ __html: pageContent.content.rendered }} />}
+
+            {/*
+              Yeah this isn't very DRY
+              I ended up copying and pasting sections because there were too many flex bugs to deal with right now
+              If you want to refactor be my guest
+            */}
+
+            {/* sidebar right and left */}
+            {ACFData && ACFData.sidebar_left && ACFData.sidebar_right && (
+              <Row flexWrap="nowrap">
+                <Column width={[1, '25%']}><section dangerouslySetInnerHTML={{ __html: ACFData.sidebar_left }} /></Column>
+
+                <Column flex="1 1 auto" width="auto">
+                  <SectionHeading>{pageContent && pageContent.title.rendered}</SectionHeading>
+                  {pageContent && <section dangerouslySetInnerHTML={{ __html: pageContent.content.rendered }} />}
+                </Column>
+
+                <Column width={[1, '25%']} > <section dangerouslySetInnerHTML={{ __html: ACFData.sidebar_right }} /></Column>
+              </Row>
+            )}
+
+            {/* sidebar right only */}
+            {ACFData && ACFData.sidebar_right && !ACFData.sidebar_left && (
+              <Row flexWrap="nowrap">
+                <Column width={[1, '75%']}>
+                  <SectionHeading>{pageContent && pageContent.title.rendered}</SectionHeading>
+                  {pageContent && <section dangerouslySetInnerHTML={{ __html: pageContent.content.rendered }} />}
+                </Column>
+
+                <Column width={[1, '25%']} > <section dangerouslySetInnerHTML={{ __html: ACFData.sidebar_right }} /></Column>
+              </Row>
+            )}
+
+            {/* sidebar left only */}
+            {ACFData && ACFData.sidebar_left && !ACFData.sidebar_right && (
+              <Row flexWrap="nowrap">
+                <Column width={[1, '25%']}><section dangerouslySetInnerHTML={{ __html: ACFData.sidebar_left }} /></Column>
+
+                <Column width={[1, '75%']}>
+                  <SectionHeading>{pageContent && pageContent.title.rendered}</SectionHeading>
+                  {pageContent && <section dangerouslySetInnerHTML={{ __html: pageContent.content.rendered }} />}
+                </Column>
+              </Row>
+            )}
+
+            {/* no sidebars */}
+            {ACFData && !ACFData.sidebar_left && !ACFData.sidebar_right && (
+              <Row flexWrap="nowrap">
+                <Column>
+                  <SectionHeading>{pageContent && pageContent.title.rendered}</SectionHeading>
+                  {pageContent && <section dangerouslySetInnerHTML={{ __html: pageContent.content.rendered }} />}
+                </Column>
+              </Row>
+            )}
+
           </Container>
         </Section>
         {ACFData && <ACF layouts={ACFData.layouts} />}
