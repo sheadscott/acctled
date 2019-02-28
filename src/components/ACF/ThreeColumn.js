e65e409de286cc265fb2726d2ea9ae83bab2a65b
Robert Ferguson <rferguso@austincc.edu>
2019-02-28 17:53:34 -0600

    add column colors to layouts

--- src/components/ACF/ThreeColumn.js
+++ src/components/ACF/ThreeColumn.js
@@ -3,6 +3,7 @@ import { Container, Row, Column as Col } from '../Grid/Grid';
 import styled, { css } from 'styled-components';
 import { Heading } from 'iw-react-elements';
 import Parser from '../Parser/Parser';
+import { Box } from '@rebass/grid';
 
 export default props => {
   return (
@@ -24,13 +25,52 @@ export default props => {
         )}
         <Row mx={[0, '-1rem']}>
           <Col width={[1, 1 / 3]} px={[0, '1rem']}>
-            <Parser>{props.layout.column_1}</Parser>
+            {props.layout.background_a === 'Color' ? (
+              <Box
+                bg={
+                  props.layout.background_a === 'Color'
+                    ? props.layout.background_color_a
+                    : null
+                }
+                p="1rem"
+              >
+                <Parser>{props.layout.column_1}</Parser>
+              </Box>
+            ) : (
+              <Parser>{props.layout.column_1}</Parser>
+            )}
           </Col>
           <Col width={[1, 1 / 3]} px={[0, '1rem']}>
-            <Parser>{props.layout.column_2}</Parser>
+            {props.layout.background_b === 'Color' ? (
+              <Box
+                bg={
+                  props.layout.background_b === 'Color'
+                    ? props.layout.background_color_b
+                    : null
+                }
+                p="1rem"
+              >
+                <Parser>{props.layout.column_2}</Parser>
+              </Box>
+            ) : (
+              <Parser>{props.layout.column_2}</Parser>
+            )}
           </Col>
           <Col width={[1, 1 / 3]} px={[0, '1rem']}>
-            <Parser>{props.layout.column_3}</Parser>
+            {props.layout.background_c === 'Color' ? (
+              <Box
+                bg={
+                  props.layout.background_c === 'Color'
+                    ? props.layout.background_color_c
+                    : null
+                }
+                p="1rem"
+              >
+                <Parser>{props.layout.column_3}</Parser>
+              </Box>
+            ) : (
+              <Parser>{props.layout.column_3}</Parser>
+            )}
           </Col>
         </Row>
       </Container>
--- src/components/ACF/TwoColumn.js
+++ src/components/ACF/TwoColumn.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Row, Column as Col } from '../Grid/Grid';
 import styled, { css } from 'styled-components';
 import { Heading } from 'iw-react-elements';
+import { Box } from '@rebass/grid';
 import Parser from '../Parser/Parser';
 
 export default props => {
@@ -24,10 +25,36 @@ export default props => {
         )}
         <Row>
           <Col width={[1, 1 / 2]} pr={[0, '1rem']}>
-            <Parser>{props.layout.column_1}</Parser>
+            {props.layout.background_a === 'Color' ? (
+              <Box
+                bg={
+                  props.layout.background_a === 'Color'
+                    ? props.layout.background_color_a
+                    : null
+                }
+                p="1rem"
+              >
+                <Parser>{props.layout.column_1}</Parser>
+              </Box>
+            ) : (
+              <Parser>{props.layout.column_1}</Parser>
+            )}
           </Col>
           <Col width={[1, 1 / 2]} pl={[0, '1rem']}>
-            <Parser>{props.layout.column_2}</Parser>
+            {props.layout.background_b === 'Color' ? (
+              <Box
+                bg={
+                  props.layout.background_b === 'Color'
+                    ? props.layout.background_color_b
+                    : null
+                }
+                p="1rem"
+              >
+                <Parser>{props.layout.column_2}</Parser>
+              </Box>
+            ) : (
+              <Parser>{props.layout.column_2}</Parser>
+            )}
           </Col>
         </Row>
       </Container>
